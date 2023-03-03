ab4535a654e8415000ecec61acb780b7e0685251
MichOchieng <MichOchieng@gmail.com>
2023-03-03 04:11:45 -0800

    Added readfile

--- main.java
+++ main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
+
 public class main {
+
+    public static void readFile(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner scanr = new Scanner(file);
+            
+            while(scanr.hasNextLine()){
+                String line = scanr.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(filename + " " + "doesn't exist.");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.print("Test");
+        readFile(args[0]);
     }
 }
