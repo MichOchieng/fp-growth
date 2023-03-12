408c82b1c472bf16b72163de3b49ff3c6cdb2ef6
Mich Ochieng <michochieng@gmail.com>
2023-03-11 23:17:43 -0800

    Added node class

--- fpgrowth.java
+++ fpgrowth.java
@@ -1,3 +1,42 @@
-class fpgrowth {
-    
+public class fpgrowth {
+    class Node {
+        private String value;
+        private Node nextNode;
+        private Node prevNode;
+        public Boolean hasNext;
+        public Boolean hasPrev;
+
+
+        public Node(){
+            this.hasNext = false;
+            this.hasPrev = false;
+            this.value   = "";
+        }
+
+        public String getValue(){
+            return this.value;
+        }
+
+        public Node getNextNode() {
+            return this.nextNode;
+        }
+
+        public Node getPrevNode() {
+            return this.prevNode;
+        }
+
+        public void setValue(String val){
+            this.value = val;
+        }
+
+        public void setNextNode(Node n){
+            this.nextNode = n;
+        }
+
+        public void setPrevNode(Node n){
+            this.prevNode = n;
+        }
+    }
+
+
 }
