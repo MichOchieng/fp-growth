631ec53443e3dc650c0438a65cf627223d0c4a02
Mich Ochieng <michochieng@gmail.com>
2023-03-11 23:18:05 -0800

    Added orderTransactions method

--- main.java
+++ main.java
@@ -9,18 +9,20 @@ public class main {
 
     private static HashMap<String, Integer> supportTable = new HashMap<String, Integer>();
     private static List<String> transactions = new ArrayList<String>();
+    private static List<List<String>> orderedTransactions = new ArrayList<List<String>>();
+    private static double support = 2.0;
+    private static double confidence;
 
     public static void readFile(String filename) {
         try {
             File file = new File(filename);
             Scanner scanr = new Scanner(file);
-
             while (scanr.hasNextLine()) {
                 String line = scanr.nextLine();
                 if (line.length() > 1) { // Skip the first line
                     String[] lineItems = line.trim().split("\t");
                     // If key present increment support otherwise initialize
-                    for (var val : lineItems[2].split("\\s+")) {
+                    for (String val : lineItems[2].split("\\s+")) {
                         supportTable.put(val, supportTable.getOrDefault(val, 0) + 1);
                     }
                     transactions.add(lineItems[2]);
@@ -32,8 +34,36 @@ public class main {
         }
     }
 
+    public static void orderTransactions(){
+        for(String transaction : transactions){
+            List<String> tempArr = new ArrayList<String>();
+            // Pruning transaction items
+            for (String str : transaction.split(" ")) {
+                // Check to see if item meets support
+                try {
+                    if (supportTable.get(str) < support) {
+                        continue;
+                    }
+                    else {
+                        tempArr.add(str);
+                    }
+                } catch (NullPointerException e) {
+                    System.out.println("Error finding '" + str + "'in hashmap.");
+                }
+            }
+            // Sort transaction
+            for (String str : tempArr) {
+                // 
+            }
+            System.out.println(tempArr);
+        }
+    }
+
     public static void main(String[] args) {
+        // Add try catch here
         readFile(args[0]);
-        System.out.print(transactions);
+        System.out.println(transactions);
+        System.out.println(supportTable);
+        orderTransactions();
     }
 }
