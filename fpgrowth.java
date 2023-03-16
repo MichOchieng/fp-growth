06c151d5f6813369c5838031f5cc4e469b10b5b0
MichOchieng <MichOchieng@gmail.com>
2023-03-15 20:43:26 -0700

    Added viewFP method, header table currently broken

--- fpgrowth.java
+++ fpgrowth.java
@@ -1,27 +1,68 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class fpgrowth {
-    private static HashMap<String, Integer> supportTable;
-    private static List<List<String>> transactions;
-    private static Node tree;
-    private static HashMap<String, Node> headerTable;
+    private HashMap<String, Integer> supportTable;
+    private List<List<String>> transactions;
+    private Node tree;
+    private HashMap<String, Node> headerTable;
+    private Integer support;
 
-    public fpgrowth(HashMap<String, Integer> supportTable, List<List<String>> transactions) {
+    public fpgrowth(HashMap<String, Integer> supportTable, List<List<String>> transactions, Integer support) {
         this.supportTable = supportTable;
         this.transactions = transactions;
         this.tree = new Node(null); // Init root of tree
         this.headerTable = new HashMap<String, Node>();
+        this.support = support;
     }
 
     public Node createFpTree() {
-        Node root = fpgrowth.tree;
+        Node root = this.tree;
         // Loop over transactions elements
         for (List<String> list : transactions) {
             insertToTree(root, list);
         }
+        System.out.println("head t");
+        System.out.println(headerTable);
         return root;
+    }
+
+    public void viewFrequenPatterns() {
+        for (String key : this.headerTable.keySet()) {
+            Integer tempSupport = this.supportTable.get(key);
+
+            if (tempSupport >= this.support) {
+                List<String> itemset = new ArrayList<String>();
+                itemset.add(key);
+
+                Node conditional = this.headerTable.get(key).getNexNode();
+                while (conditional != null) {
+                    List<String> conditionalItems = new ArrayList<String>();
+                    Node current = conditional;
+                    Integer conditionalSupport = current.getSupport();
+                    Set<Node> visited = new HashSet<Node>();
+                    while (current.getParent().getValue() != null) {
+                        if (!visited.contains(current)) {
+                            visited.add(current);
+                            conditionalItems.add(current.getValue());
+                        }
+                        current = current.getParent();
+                    }
+                    if (!conditionalItems.isEmpty()) {
+                        Collections.reverse(conditionalItems);
+                        conditionalItems.addAll(itemset);
+                        System.out.println(conditionalItems.toString() + " : " + conditionalSupport);
+                    }
+                    conditional = conditional.getNexNode();
+                }
+                System.out.println(itemset.toString() + " : " + tempSupport);
+            }
+        }
     }
 
     void insertToTree(Node node, List<String> transaction) {
