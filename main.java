312e33c43e0b3f9a2e07c59733d06abff785f7d7
MichOchieng <MichOchieng@gmail.com>
2023-03-15 22:42:26 -0700

    Added error handling

--- main.java
+++ main.java
@@ -18,16 +18,17 @@ public class main {
     private static List<String[]> transactions = new ArrayList<String[]>();
     private static List<List<String>> orderedTransactions = new ArrayList<List<String>>();
     // Params
-    private static double support = 2.0;
-    private static double confidence;
+    private static Integer support;
 
-    public static void readFile(String filename) {
+    public static void readFile(String filename, Integer sup) {
         try {
+            support = sup;
             File file = new File(filename);
             Scanner scanr = new Scanner(file);
+            int i = 0;
             while (scanr.hasNextLine()) {
                 String line = scanr.nextLine();
-                if (line.length() > 1) { // Skip the first line
+                if (i > 0) { // Skip the first line
                     String[] lineItems = line.trim().split("\t");
                     // If key present increment support otherwise initialize
                     for (String val : lineItems[2].split("\\s+")) {
@@ -35,6 +36,7 @@ public class main {
                     }
                     transactions.add(lineItems[2].split(" "));
                 }
+                i++;
             }
             scanr.close();
         } catch (FileNotFoundException e) {
@@ -42,6 +44,8 @@ public class main {
         }
 
         // Sort the Suport Table
+        // Creates a temp list that can use the support table supports as keys to sort
+        // transactions later on in correct order
         List<Entry<String, Integer>> tempList = new LinkedList<>(supportTable.entrySet());
 
         Collections.sort(tempList, new Comparator<Entry<String, Integer>>() {
@@ -67,6 +71,7 @@ public class main {
         }
 
         // Pruning and Sorting transactions
+        // Uses the tempArr from before
         for (String[] string : transactions) {
             List<String> tempArr = new ArrayList<String>();
             for (String string2 : string) {
@@ -80,20 +85,25 @@ public class main {
             }
             // Awkward lambda for sorting transactions in descending order
             Collections.sort(tempArr, (x, y) -> supportTable.get(y).compareTo(supportTable.get(x)));
+            // Add ordered transaction to list of transactions
             orderedTransactions.add(tempArr);
         }
 
     }
 
     public static void main(String[] args) {
-        // Add try catch here
-        readFile(args[0]);
-
-        System.out.println(supportTable);
-        System.out.println(orderedTransactions);
-        fpgrowth fp = new fpgrowth(supportTable, orderedTransactions);
+        // Error handling for inputs
+        try {
+            readFile(args[0], Integer.parseInt(args[1]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Make sure to add the filename and support level!");
+        } catch (NumberFormatException e) {
+            System.out.println("Make sure to enter a number as the second parameter!");
+        }
+        // Create fpTree
+        fpgrowth fp = new fpgrowth(supportTable, orderedTransactions, support);
         fp.createFpTree();
-        // fp.viewTree();
-        // orderTransactions();
+        // Print patterns
+        fp.viewFrequentPatterns();
     }
 }
